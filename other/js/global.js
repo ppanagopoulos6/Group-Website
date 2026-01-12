f69b44a2887f2cd52929b8426bacf25302772df4
ppanagopoulos6 <ppanagopoulos6@gmail.com>
2026-01-13 01:45:05 +0200

    Add files via upload

---
+++ other/js/global.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",function(){
+var path = window.location.pathname;
+var homeLink = "../../Site/html/continents.html";
+
+var btn = document.createElement("a");
+btn.innerText = "<-- Continents"
+btn.href = homeLink
+btn.className = "global-continent-btn";
+var style = document.createElement("style");
+    style.innerHTML = `
+        .global-continent-btn {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            background-color: #0d0d0eff;
+            color: white;
+            padding: 15px 25px;
+            border-radius: 50px;
+            text-decoration: none;
+            font-family: sans-serif;
+            font-weight: bold;
+            box-shadow: 0 4px 10px rgba(0,0,0,0.3);
+            z-index: 9999;
+            transition: transform 0.3s, background 0.3s;
+        }
+        .global-continent-btn:hover {
+            color: black;
+            transform: scale(1.1);
+            background-color: #e4e4e4ff;
+        }
+    `;
+    document.head.appendChild(style);
+    document.body.appendChild(btn);
+});
